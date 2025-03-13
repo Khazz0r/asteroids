0b0ba8bd81d986c2c0b47224f422b7310385c36a
Khazz0r <kaw3198@gmail.com>
2025-03-12 20:31:26 -0400

    Created player object onto screen and added movement in through a circleshape and player file and modified constants and main.py

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
+    # Initialization
     pygame.init()
         
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
+    Player.containers = (updatable, drawable)
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill((0, 0, 0))
-        
+
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
+
+        # Set an integer in seconds for delta time and limit to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
